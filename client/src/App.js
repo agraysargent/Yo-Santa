278a3e87a52f0bb23b7c8b79ab2b93f9a6067897
Angelica Sargent <agraysargent@gmail.com>
2021-08-24 21:18:51 -0400

    stylingbases

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,15 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
+import Packages from "./pages/Packages";
 
 function App() {
   return (
     <div className="App">
+      <Home/>
+      <SignUp/>
+      <Packages/>
       <header className="App-header">
         <p>
           Edit <code>src/App.js</code> and save to reload.
