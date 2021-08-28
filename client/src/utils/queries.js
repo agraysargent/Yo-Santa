dcc121908ce412255715ec50e62636fef12c9104
James O'Keefe <jbo88@att.net>
2021-08-28 12:52:37 -0400

    updated b/e seeds, typeDefs, user and order models. updated f/e utils (actions, auth, GlobalState, mutations, queries, reducers).

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,1 +1,30 @@
-// queries
+import { gql } from '@apollo/client';
+
+export const QUERY_CHECKOUT = gql`
+  query getCheckout($events: [ID]!) {
+    checkout(events: $events) {
+      session
+    }
+  }
+`;
+
+// For icebox profile functionality
+export const QUERY_USER = gql`
+  {
+    user {
+      firstName
+      lastName
+      orders {
+        _id
+        purchaseDate
+        events {
+          _id
+          name
+          description
+          duration
+          price
+        }
+      }
+    }
+  }
+`;
--- server/models/Order.js
+++ server/models/Order.js
@@ -7,7 +7,7 @@ const orderSchema = new Schema({
     type: Date,
     default: Date.now
   },
-  event: [
+  events: [
     {
       type: Schema.Types.ObjectId,
       ref: 'Event'
